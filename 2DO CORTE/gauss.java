fdea7b5848045509a6efb3688c8ff4dae8d86fa4
Mauricio Molina <maupena9@gmail.com>
2023-09-12 23:08:22 -0500

    organizando y subiendo 2do corte

---
+++ 2DO CORTE/gauss.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SegundoCorte;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.Scanner;
+
+public class EliminacionGauss {
+
+    static int m, n;
+    static double[][] M;
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("Digite ecuaciones: ");
+        n = read.nextInt();
+        System.out.print("Digite incognitas: ");
+        m = read.nextInt();
+
+        M = new double[n][m + 1];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("\n Digite número en la posición[" + i + "," + j + "]:");
+                M[i][j] = read.nextDouble();
+            }
+        }
+
+        System.out.println("Matriz generada");
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("\t" + M[i][j]);
+            }
+            System.out.println("\n");
+        }
+
+        for (int i = 1; i < n; i++) {
+            double div = M[i][i];
+            for (int j = 1; j <= m; j++) {
+                M[i][j] /= div;
+            }
+
+            for (int k = i + 1; k < n; k++) {
+                double pv = (-1) * M[k][i];
+                for (int j = 1; j <= m; j++) {
+                    M[k][j] = pv * M[i][j] + M[k][j];
+                }
+            }
+        }
+
+        System.out.println("Matriz resultante");
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("\t" + M[i][j]);
+            }
+            System.out.println("\n");
+        }
+    }
+}
---
+++ 2DO CORTE/gauss.js
@@ -0,0 +1,7 @@
+var m,n
+var float[][] M
+
+var m = prompt("Digite ecuaciones: ")
+var n = prompt("Digite incognitas: ")
+
+M = [n][m+1]
